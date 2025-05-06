dab432f875bf111f8caaeffc94058973b4e72c92
Nick Paris <nick@onchainden.com>
2025-05-05 20:57:36 -0400

    [update] POOL CONNECTION

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,12 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { sql } from "@vercel/postgres";
+import { createClient } from "@vercel/postgres";
+import { env } from "~/env";
 
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+// Create a pooled connection with the DATABASE_URL
+const connection = createClient({
+  connectionString: env.DATABASE_URL,
+});
+
+export const db = drizzle(connection, { schema });
